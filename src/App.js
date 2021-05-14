07f0593efd28dd00b06036e2933214b78843efbe
rsaleh <raihan.saleh@gmail.com>
2021-05-14 12:42:34 -0400

    First commit: created react up and base file structure

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react'
+import { Context } from './components/Context'
+import { Route, Switch } from 'react-router-dom'
+import Bootstrap from 'bootstrap/dist/css/bootstrap.css'
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
