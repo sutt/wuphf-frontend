3a300fd64f61f2dd81f3df5cbe3c6a50e8ee1e85
rsaleh <raihan.saleh@gmail.com>
2021-05-17 11:24:28 -0400

    lets try this again... newsfeed render

--- src/components/NewsFeed.jsx
+++ src/components/NewsFeed.jsx
@@ -1,9 +1,31 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useContext } from 'react';
+import { Context } from './Context';
 
 function Newsfeed(props) {
+  const {posts, setPosts} = useContext(Context)
+
+  // let sortedPosts = [...posts]
+  let sortedPosts = [
+    {author: 'Raihan', content: 'post1'},
+    {author: 'MAx', content: 'post2'},
+    {author: 'Justin', content: 'post3'},
+    {author: 'Evan', content: 'post4'},
+  ]
+
+  // creating <div> tags for each post to be rendered.
+  const newsFeed = sortedPosts.map(post => {
+    return (
+      <div>
+        <h4>{post.author}</h4>
+        <p>{post.content}</p>
+      </div>
+    )
+  })
+
   return (
     <div>
-      Newsfeed rendering
+      {newsFeed}
     </div>
   );
 }
