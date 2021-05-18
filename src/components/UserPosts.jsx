269682ec2a02d0f5c92f980320a306a9dbfe028f
Juswhite814 <juswhite814@gmail.com>
2021-05-18 16:29:12 -0400

    modal open/close complete

--- src/components/UserPosts.jsx
+++ src/components/UserPosts.jsx
@@ -1,8 +1,9 @@
 import axios from 'axios'
-import React, { useContext } from "react";
+import React, { useState, useContext } from "react";
+import Modal from 'react-modal'
 import { Context } from './Context';
 
-function UserPosts() {
+function UserPosts(props) {
   const {posts, baseURL, getPosts, user } = useContext(Context)
 
   const sortedPosts = [...posts].filter(post => post.author === user.username)
@@ -20,15 +21,35 @@ function UserPosts() {
     getPosts()
   }
 
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const setModaltoOpenToTrue = () => {
+    setModalIsOpen(true)
+    console.log(modalIsOpen)
+  }
+  const setModalIsOpenToFalse =() => {
+    setModalIsOpen(false)
+}
+
+
+
   // creating <div> tags for each post to be rendered.
   const profileFeed = sortedPosts.map(post => {
     return (
       <div key={post._id}>
         <h4>{post.author}</h4>
         <p>{post.content}</p>
-        <button>Edit</button>
+        <button onClick={setModaltoOpenToTrue} name={post._id}>Edit</button>
         <button onClick={deleteWuphf} name={post._id}>Delete</button>
-      </div>
+        <Modal isOpen={modalIsOpen} ariaHideApp={false}>
+          <button onClick={setModalIsOpenToFalse}>x</button>
+          {/* This will cause an error because of needing an onchange function */}
+          {/* <form><textarea type='text' value={post.content} name='post'/> */}
+          {/* When using post.content, only the last comment will render in the Modal */}
+          {/* <h1>{post.content}</h1> */}
+          <h1>Hello</h1>
+          {/* </form> */}
+        </Modal>
+        </div>
     )
   })
 
